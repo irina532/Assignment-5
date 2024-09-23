9db46a92e65fcd8be1a1f59732b32295313455de
Erina-Akter <erinaakter2001@gmail.com>
2024-09-23 19:29:42 +0600

    completed blog page

--- js/donate.js
+++ js/donate.js
@@ -1,106 +1,128 @@
 
- src="https://cdn.tailwindcss.com"
+src = "https://cdn.tailwindcss.com"
 //donating noakhali
-document.getElementById('noakhali-donate-btn').addEventListener('click', function (event){
-event.preventDefault;
+document.getElementById('noakhali-donate-btn').addEventListener('click', function (event) {
+    event.preventDefault;
 
-const inputDonationNoakhali= getInput('input-noakhali');
-if(!inputDonationNoakhali || isNaN(inputDonationNoakhali)){
-    return;
-}
+    const inputDonationNoakhali = getInput('input-noakhali');
+    if (!inputDonationNoakhali || isNaN(inputDonationNoakhali)) {
+         alert('Invalid Donation Amount')
+        return;
+    }
 
-const bdtNoakhali =getBDT('noakhali-bdt');
-const bdt= calculateDonation(inputDonationNoakhali,bdtNoakhali);
-if(typeof(bdt)==='number'){ 
-document.getElementById('noakhali-bdt').innerText=bdt;
-const inputDonationNoakhali= getInput('input-noakhali');
-const div = document.createElement('div');
-div.classList.add('border', 'rounded-xl','px-5','py-5')
+    const bdtNoakhali = getBDT('noakhali-bdt');
+    const bdt = calculateDonation(inputDonationNoakhali, bdtNoakhali);
+    if (typeof (bdt) === 'number') {
+        document.getElementById('noakhali-bdt').innerText = bdt;
+        const inputDonationNoakhali = getInput('input-noakhali');
+        const div = document.createElement('div');
+        div.classList.add('border', 'rounded-xl', 'px-5', 'py-5')
 
-const date =Date();
+        const date = Date();
 
-div.innerHTML=`
+        div.innerHTML = `
 <h4 class="text-xl font-bold">${inputDonationNoakhali} Taka is Donated for Famine-2024 at Noakhlai</h4>
 <p class="text-black opacity-70 text-sm">${date}</p>`;
-document.getElementById('history').appendChild(div);
+        document.getElementById('history').appendChild(div);
 
 
-}
-    
+    }
+
 })
 
 //donating feni
-document.getElementById('feni-donate-btn').addEventListener('click', function (event){
+document.getElementById('feni-donate-btn').addEventListener('click', function (event) {
     event.preventDefault;
-    const inputDonationFeni =getInput('input-feni');
-    if(!inputDonationFeni || isNaN(inputDonationFeni)){
+    const inputDonationFeni = getInput('input-feni');
+    if (!inputDonationFeni || isNaN(inputDonationFeni)) {
         return;
     }
     const bdtFeni = getBDT('feni-bdt');
-    const bdt = calculateDonation(inputDonationFeni,bdtFeni);
-    if(typeof(bdt)==='number'){ 
-    document.getElementById('feni-bdt').innerText=bdt;
+    const bdt = calculateDonation(inputDonationFeni, bdtFeni);
+    if (typeof (bdt) === 'number') {
+        document.getElementById('feni-bdt').innerText = bdt;
+        const inputDonationFeni = getInput('input-feni');
+        const div = document.createElement('div');
+        div.classList.add('border', 'rounded-xl', 'px-5', 'py-5')
+
+        const date = Date();
+
+        div.innerHTML = `
+<h4 class="text-xl font-bold">${inputDonationFeni} Taka is Donated for Famine-2024 at Feni</h4>
+<p class="text-black opacity-70 text-sm">${date}</p>`;
+        document.getElementById('history').appendChild(div);
+
     }
-    
-    })
+
+})
 
 //donating quota movement
-document.getElementById('quota-donate-btn').addEventListener('click', function (event){
+document.getElementById('quota-donate-btn').addEventListener('click', function (event) {
     event.preventDefault;
-    const inputDonationQuota =getInput('input-quota');
-    if(!inputDonationQuota || isNaN(inputDonationQuota)){
+    const inputDonationQuota = getInput('input-quota');
+    if (!inputDonationQuota || isNaN(inputDonationQuota)) {
         return;
     }
-    const bdtQuota =getBDT('quota-bdt');
-    const bdt= calculateDonation(inputDonationQuota,bdtQuota);
-    if(typeof(bdt)==='number'){ 
-    document.getElementById('quota-bdt').innerText=bdt;
+    const bdtQuota = getBDT('quota-bdt');
+    const bdt = calculateDonation(inputDonationQuota, bdtQuota);
+    if (typeof (bdt) === 'number') {
+        document.getElementById('quota-bdt').innerText = bdt;
+        const inputDonationQuota = getInput('input-quota');
+        const div = document.createElement('div');
+        div.classList.add('border', 'rounded-xl', 'px-5', 'py-5')
+
+        const date = Date();
+
+        div.innerHTML = `
+<h4 class="text-xl font-bold">${inputDonationQuota} Taka is Donated for Famine-2024 at Feni</h4>
+<p class="text-black opacity-70 text-sm">${date}</p>`;
+        document.getElementById('history').appendChild(div);
     }
-    })
+})
 
 //getting input value by id
-function getInput(id){
-    
-     
-  const inputAmount = document.getElementById(id);
-  const inputValue =inputAmount.value;
+function getInput(id) {
 
-  return inputValue; 
-    
+
+    const inputAmount = document.getElementById(id);
+    const inputValue = inputAmount.value;
+
+    return inputValue;
+
 }
 
 //getting bdt
-function getBDT(id){
-    const bdt =document.getElementById(id).innerText;
+function getBDT(id) {
+    const bdt = document.getElementById(id).innerText;
 
     return bdt;
 
 }
 //calculating donation
-function calculateDonation(inputAmount, bdtAmount){
-    const amount =document.getElementById('main-amount').innerText;
+function calculateDonation(inputAmount, bdtAmount) {
+    const amount = document.getElementById('main-amount').innerText;
 
-    if(isNaN(inputAmount)){
+    if (isNaN(inputAmount) || inputAmount < 0) {
         alert('Invalid Donation Amount');
-        
+
     }
-    else{
-        const amountNumber =parseFloat(amount);
-        const inputNumber =parseFloat(inputAmount);
-        const bdtAmountNumber =parseFloat(bdtAmount);
-        const balance= amountNumber-inputNumber;
-        if(balance<0){
+    else {
+        const amountNumber = parseFloat(amount);
+        const inputNumber = parseFloat(inputAmount);
+        const bdtAmountNumber = parseFloat(bdtAmount);
+        const balance = amountNumber - inputNumber;
+        if (balance < 0) {
             alert('Invalid Donation Amount');
         }
-        else{
-            const bdtBalance =inputNumber + bdtAmountNumber;
-            document.getElementById('main-amount').innerText=balance;
-        
+        else {
+            const bdtBalance = inputNumber + bdtAmountNumber;
+            document.getElementById('main-amount').innerText = balance;
+
             return bdtBalance;
 
         }
-        
-        
+
+
     }
 
 }
