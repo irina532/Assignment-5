e8ea341441a58655552fea94a45e5d4062e525b3
Erina-Akter <erinaakter2001@gmail.com>
2024-09-23 22:36:05 +0600

    redirecting home to blog

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,5 @@
 //code for redirecting to blog page when clicking blog button
 document.getElementById('blog-btn').addEventListener('click',function(){
     window.location.href = 'blogs.html';
-})
+});
+
---
+++ js/home.js
@@ -0,0 +1,4 @@
+//code for redirecting to homepage
+document.getElementById('home-btn').addEventListener('click',function(){
+    window.location.href = 'index.html';
+});
